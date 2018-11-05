12f3a7b11d74dfe45920047ab5581d795e61b84a
Patrick Miziewicz <patrick.miziewicz@gmail.com>
2018-11-05 20:53:31 +1100

    Added missing shebang

--- nginx/analytics/calculate_buffer_payload_size.sh
+++ nginx/analytics/calculate_buffer_payload_size.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 
+# Calculates the average and maximum buffer message sizes
 awk '($9 ~ /200/) { i++;sum+=$10;max=$10>max?$10:max; } END { printf("Maximum: %d\nAverage: %d\n",max,i?sum/i:0); }' /var/log/nginx/access.log

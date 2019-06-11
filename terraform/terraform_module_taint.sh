21e1d85d970a6f8b412186e07b1267f3437e92a0
Patrick Miziewicz <36363132+patmizi@users.noreply.github.com>
2019-06-11 10:16:34 +1000

    Update terraform_module_taint.sh

--- terraform/terraform_module_taint.sh
+++ terraform/terraform_module_taint.sh
@@ -4,7 +4,7 @@ function taint_module() {
     MODULE_NAME="$1"
     EXTRA_ARGS="${@:2}"
 
-    for resource in "terraform show -module-depth=1 ${EXTRA_ARGS} | grep module.${MODULE_NAME} | tr -d ':' | sed -e 's/module.${MODULE_NAME}.//'"; do
+    for resource in `terraform show -module-depth=1 ${EXTRA_ARGS} | grep module.${MODULE_NAME} | tr -d ':' | sed -e 's/module.${MODULE_NAME}.//'`; do
         terraform taint -module ${MODULE_NAME} ${resource}
     done
 }
